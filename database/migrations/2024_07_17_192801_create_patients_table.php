c8274818387799a39a4bb0ef593a9c4657cdcdcf
Phuc Tran <glup3.tran@gmail.com>
2024-07-18 23:09:25 +0200

    add quickstart code

--- database/migrations/2024_07_17_192801_create_patients_table.php
+++ database/migrations/2024_07_17_192801_create_patients_table.php
@@ -4,21 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up(): void
     {
-        Schema::create('patients', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        Schema::create(
+            'patients',
+            function (Blueprint $table) {
+                $table->id();
+                $table->date('date_of_birth');
+                $table->string('name');
+                $table->foreignId('owner_id')
+                    ->constrained('owners')
+                    ->cascadeOnDelete();
+                $table->string('type');
+                $table->timestamps();
+            }
+        );
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
     public function down(): void
     {
